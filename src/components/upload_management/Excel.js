ea2b01f3a5fab40f1b3a4531b245c3e8bd51b5e8
hakiimmislam <hakiimmislam@gmail.com>
2019-01-31 23:16:41 +0800

    added new screen for upload excel and photo

---
+++ src/components/upload_management/Excel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Excel extends Component {
+    render() {
+        return (
+            <div>Excel</div>
+        );
+    }
+}
+
+export default Excel;
